748f5d0bfa7bc7d319db1f394a0522951a1ff6e3
jala88 <cbarquero70286@ufide.ac.cr>
2024-06-02 10:40:33 -0600

    Creacion de proyecto y de Git.Ignore
    Se crea el proyecto,git ignore y se liga a repositorio en GitHub

---
+++ JN_API/JN_API/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JN_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/[controller]")]
+        public IActionResult ConsultarUsuarios() 
+        {
+            var resultado = 750000;
+            return Ok(resultado);
+        }
+    }
+}
